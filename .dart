9656408d5323f3cdf92cd55dc2c52c99a52739ac
higorhp <112891205+higorhp@users.noreply.github.com>
2023-04-12 15:03:45 -0300

    create .dart

---
+++ .dart
@@ -0,0 +1,18 @@
+int void (){
+ 
+  String nome = "Laranja";
+  double peso = 100.2
+  String corFruta = "Verde e Amarela"
+  String sabor = "Doce e cítrica";
+  int diasDesdecolheita = 20;
+  bool isMadura;
+  
+  if (diasDesdeColheita >= 30){
+    isMadura = true; 
+    
+  }else{
+   isMadura = false; 
+    
+  }
+  
+}
